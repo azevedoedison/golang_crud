47e4f58257d34f1718d5ae15a7f7b65a06fb8521
azevedoedison <azevedoedison@gmail.com>
2023-12-16 16:43:18 -0300

    CRUD_GO - creation of routes

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
-	"os"
 
+	"github.com/azevedoedison/golang_crud/src/controller/routes"
+	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
 )
 
@@ -13,5 +13,11 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(os.Getenv("TEST"))
+
+	router := gin.Default()
+
+	routes.InitRoutes(&router.RouterGroup)
+	if err := router.Run(":8080"); err != nil {
+		log.Fatal(err)
+	}
 }
