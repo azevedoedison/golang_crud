7ba2a46f60df617ea75bd5bac5b9d2fdcc0538cb
azevedoedison <azevedoedison@gmail.com>
2023-12-18 15:44:19 -0300

    CRUD_GO - Json validation

--- src/controller/model/request/user_request.go
+++ src/controller/model/request/user_request.go
@@ -1,8 +1,8 @@
 package request
 
 type UserRequest struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Name     string `json:"name"`
-	Age      int8   `json:"age"`
+	Email    string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required,containsany=!@#?"`
+	Name     string `json:"name" binding:"required,max=10"`
+	Age      int8   `json:"age" binding:"required"`
 }
