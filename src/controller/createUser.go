c07e9489eb2fc8581c08eeba60b1d7d0679f373f
azevedoedison <azevedoedison@gmail.com>
2023-12-18 09:35:56 -0300

    CRUD_GO - Creation of request and response

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,11 +1,23 @@
 package controller
 
 import (
+	"fmt"
+
 	rest_err "github.com/azevedoedison/golang_crud/src/configuration/rest_err"
+	"github.com/azevedoedison/golang_crud/src/controller/model/request"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	err := rest_err.NewBadRequestError("Você chamou a rota de forma errada")
-	c.JSON(err.Code, err)
+	var userRequest request.UserRequest
+
+	err := c.ShouldBindJSON(&userRequest)
+	if err != nil {
+		restErr := rest_err.NewBadRequestError(
+			fmt.Sprintf("There are some incorrect fields, error: %v", err.Error()),
+		)
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+	fmt.Println(userRequest)
 }
