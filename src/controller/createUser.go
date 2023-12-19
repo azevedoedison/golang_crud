b39723460df67de5612a1d88626d3154a12bf7fa
azevedoedison <azevedoedison@gmail.com>
2023-12-18 22:50:05 -0300

    CRUD_GO - Customizing the validators

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -2,22 +2,33 @@ package controller
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 
-	rest_err "github.com/azevedoedison/golang_crud/src/configuration/rest_err"
+	"github.com/azevedoedison/golang_crud/src/configuration/validation"
 	"github.com/azevedoedison/golang_crud/src/controller/model/request"
+	"github.com/azevedoedison/golang_crud/src/controller/model/response"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
+
+	log.Println("Init CreateUser Controller")
 	var userRequest request.UserRequest
 
 	err := c.ShouldBindJSON(&userRequest)
 	if err != nil {
-		restErr := rest_err.NewBadRequestError(
-			fmt.Sprintf("There are some incorrect fields, error: %v", err.Error()),
-		)
+		log.Printf("Error tring to marshal object, error=%s\n", err.Error())
+		restErr := validation.ValidateUserError(err)
 		c.JSON(restErr.Code, restErr)
 		return
 	}
 	fmt.Println(userRequest)
+	response := response.UserResponse{
+		ID:    "test",
+		Name:  userRequest.Name,
+		Email: userRequest.Email,
+		Age:   userRequest.Age,
+	}
+	c.JSON(http.StatusOK, response)
 }
